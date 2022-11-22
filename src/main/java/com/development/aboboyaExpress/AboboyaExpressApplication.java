573de87b7ca0492a28117958446892303fc87b16
Agada <agadap46@gmail.com>
2022-11-22 19:59:17 +0000

    Iniital Commit: Base Project Structure

---
+++ src/main/java/com/development/aboboyaExpress/AboboyaExpressApplication.java
@@ -0,0 +1,13 @@
+package com.development.aboboyaExpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboboyaExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AboboyaExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/development/aboboyaExpress/AboboyaExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.aboboyaExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboboyaExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
